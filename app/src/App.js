fb9ff8578b9c3d4635a198bb2a4a1071709b8b5a
Flymax <li.zhming@outlook.com>
2022-02-02 16:35:28 +0800

    mint nft

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
+import CandyMachine from './CandyMachine';
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
@@ -75,6 +76,8 @@ const App = () => {
           <p className="sub-text">NFT drop machine with fair mint</p>
           {/* Render your connect to wallet button right here */}
           {!walletAddress && renderNotConnectedContainer()}
+          {walletAddress && <CandyMachine walletAddress={window.solana} />}
+
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
